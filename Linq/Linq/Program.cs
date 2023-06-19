6e476ef3df84258ab0178bf38fbd648c6b222308
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-06-19 11:48:47 +0400

    Add files via upload

---
+++ Linq/Linq/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> productList = new Dictionary<string, int>();
+
+            productList.Add("Hard Disk", 1280);
+            productList.Add("Printer", 3000);
+            productList.Add("Keyboard", 3500);
+         
+            //Method 1
+            var search = from x in productList
+                         where x.Key.Contains("Disk")
+                         select x;
+            //Method 2
+            //var search = productList.Where(p => p.Key.Contains("Disk"));
+
+            foreach (var result in search)
+            {
+                Console.WriteLine("Product Name: {0}, Price: {1}", result.Key, result.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    }
